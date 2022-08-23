3a2f87ce98079229f7143c1fcf74604d428fd6ad
saidsoud72 <said.omar@student.moringaschool.com>
2022-08-23 10:42:41 +0300

    dom test assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// Write your code here!
+main.remove()
+
+let newHeader=document.createElement('h1');
+
+newHeader.id=("victory");
+
+newHeader.innerHTML= "<h1> Ivan is the champion </h1>" 
